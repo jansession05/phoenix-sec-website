79d9588d584cf19727209092da9037bc1bac82eb
Jan <jancarandell@gmail.com>
2026-05-06 01:47:58 +0200

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    // Desactiva la validación de ESLint durante el build para evitar errores en Vercel
-    ignoreDuringBuilds: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/research',
-        destination: '/ctf-research',
-        permanent: true,
-      },
-    ]
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
